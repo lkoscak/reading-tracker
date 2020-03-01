030df6f77379b1c8dfc73313541e7dc7db8f61fd
unknown <luka.koscak96@gmail.com>
2020-03-01 15:01:39 +0100

    Added context class for accessing database

---
+++ book-tracker/ReadingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_tracker
+{
+    public class ReadingContext : DbContext
+    {
+        public ReadingContext(DbContextOptions<ReadingContext> options) : base(options) { }
+
+       
+    }
+}
