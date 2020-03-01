1f98f31ff0a6ecd69cca5712621b4efb852735e6
unknown <luka.koscak96@gmail.com>
2020-03-01 17:55:53 +0100

    Created controller class for managing author entities

---
+++ book-tracker/Controllers/AuthorsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using book_tracker;
+using book_tracker.Models;
+
+namespace book_tracker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ReadingContext _context;
+
+        public AuthorsController(ReadingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Authors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        // GET: Authors/id
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Author>> GetAuthorById(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
+        // PUT: Authors/id
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAuthor(int id, Author author)
+        {
+
+            var authorToUpdate = await _context.Authors.FindAsync(id);
+            author.AuthorID = authorToUpdate.AuthorID;
+            _context.Entry(authorToUpdate).CurrentValues.SetValues(author);
+            _context.Authors.Update(authorToUpdate);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: Authors
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Author>> PostAuthor(Author author)
+        {
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAuthorById", new { id = author.AuthorID }, author);
+        }
+
+        // DELETE: Authors/id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Author>> DeleteAuthor(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _context.Authors.Remove(author);
+            await _context.SaveChangesAsync();
+
+            return author;
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.AuthorID == id);
+        }
+    }
+}
--- book-tracker/ReadingContext.cs
+++ book-tracker/ReadingContext.cs
@@ -11,9 +11,9 @@ namespace book_tracker
     {
         public ReadingContext(DbContextOptions<ReadingContext> options) : base(options) { }
 
-        DbSet<Author> Authors { get; set; }
-        DbSet<Rating> Ratings { get; set; }
-        DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Book> Books { get; set; }
        
     }
 }
